fdacc982df40d4c31647641a734183ffd384143d
I563855 <pavel.ivanov05@sap.com>
2026-05-11 22:15:25 +0300

    init

---
+++ src/main/java/com/projects/cicd/CiCdServiceApplication.java
@@ -0,0 +1,13 @@
+package com.projects.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projects/cicd/CiCdServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
