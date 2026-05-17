3095ec3d7c9ab086340e6e68ebd6dd5f3107c51a
I563855 <pavel.ivanov05@sap.com>
2026-05-17 16:50:58 +0300

    add navigation between views

--- src/main/resources/static/controller/App.controller.js
+++ src/main/resources/static/controller/App.controller.js
@@ -1,11 +1,41 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function (Controller) {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+], function (Controller, JSONModel) {
     "use strict";
 
     return Controller.extend("cicd.controller.App", {
 
         onInit: function () {
+            var viewModel = new JSONModel({
+                selectedTab: "repositories"
+            });
+            this.getView().setModel(viewModel);
+
+            var router = this.getOwnerComponent().getRouter();
+            router.attachRouteMatched(this.onRouteMatched, this);
+        },
+
+        onRouteMatched: function (event) {
+            var routeName = event.getParameter("name");
+            var viewModel = this.getView().getModel();
+
+            if (routeName === "repositories") {
+                viewModel.setProperty("/selectedTab", "repositories");
+            } else if (sRouteName === "secrets") {
+                viewModel.setProperty("/selectedTab", "secrets");
+            }
+        },
+
+        onTabSelect: function (event) {
+            var key = event.getParameter("key");
+            var router = this.getOwnerComponent().getRouter();
+
+            if (key === "repositories") {
+                router.navTo("repositories");
+            } else if (key === "secrets") {
+                router.navTo("secrets");
+            }
         }
 
     });
